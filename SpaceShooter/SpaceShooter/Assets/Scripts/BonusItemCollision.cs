2ac804bd8f6f1209a8ba91bfaa0ea2bb6bca9518
Sutanto <sutanto.1010@gmail.com>
2015-11-09 18:57:46 +0700

    Add missing files

---
+++ SpaceShooter/SpaceShooter/Assets/Scripts/BonusItemCollision.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusItemCollision : MonoBehaviour
+{
+  private GameObject playerGameObject;
+  private Done_PlayerController playerController;
+	// Use this for initialization
+	void Start () {
+	  playerGameObject = GameObject.FindWithTag("Player");
+	  playerController = playerGameObject.GetComponent<Done_PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  private void OnTriggerEnter(Collider other)
+  {
+    if (other.tag == "Player")
+    {
+      playerController.SpeedUpFire(0.1f);
+      Destroy(gameObject);
+    }
+  }
+}
